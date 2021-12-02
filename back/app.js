f298646b90a5d6128ad067cb7b44e4745d314073
amitnahtomi <amit.nahtomi@gmail.com>
2021-12-02 11:46:38 +0200

    react and server templates

---
+++ back/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+  });
