fc989185df535af868f7767695991ca61ed918ed
amitnahtomi <amit.nahtomi@gmail.com>
2021-12-04 16:13:41 +0200

    more style not done

--- chat-room/src/chatRoom.js
+++ chat-room/src/chatRoom.js
@@ -13,6 +13,7 @@ export default function ChatRoom(props) {
                 },
                 body: JSON.stringify({"user": props.nickName, "time": new Date().toLocaleString().slice(10, 16) ,"msg": massage.current.value})
             })
+        massage.current.value = "";
         }
         const logOutClick = async () => {
             const response = await fetch("http://localhost:8080/connect" ,{
@@ -24,26 +25,27 @@ export default function ChatRoom(props) {
                 body: JSON.stringify({"user": props.nickName})
             })
         }
-        return <div>
+        return <div style={logStyle}>
         <ul style={msgListStyle}>
             {props.massages.map((msg)=>{
                 return <li style={msgStyle}><span style={senderStyle}>{msg.user}: </span><br /><span>{msg.msg}</span><label style={timeStyle}>{msg.time}</label></li>
             })}
         </ul>
-        <ul>
+        <ul style={usersListStyle}>
         {props.participants.map((participant)=>{
                 return <li>{participant}</li>
             })}
         </ul>
-        <input ref={massage} placeholder='type your massage'></input>
-        <button onClick={sendClick}>send</button>
-        <button onClick={logOutClick}><Link to='/'>log out</Link></button>
+        <input style={inputStyle} ref={massage} placeholder='type your massage'></input>
+        <button style={buttonStyle} onClick={sendClick}>send</button>
+        <button style={buttonStyle} onClick={logOutClick}><Link style={linkStyle} to='/'>log out</Link></button>
     </div>
 }
 
 const msgListStyle = {
     listStyleType: "none",
-    fontSize: "20px"
+    fontSize: "20px",
+    width: "200px"
 }
 
 const msgStyle = {
@@ -51,8 +53,7 @@ const msgStyle = {
     borderRadius: "10px",
     width: "100px",
     paddingLeft: "5px",
-    marginTop: "10px"
-
+    marginTop: "10px",
 }
 
 const senderStyle = {
@@ -63,4 +64,47 @@ const senderStyle = {
 const timeStyle = {
     color: "darkgrey",
     fontSize: "8px"
+}
+
+const linkStyle = {
+    textDecoration: "none",
+    color: "blue"
+}
+
+const logStyle = {
+    textAlign: "center",
+    paddingTop: "50px",
+    fontSize: "45px",
+    color: "LightSkyBlue"
+}
+
+const inputStyle = {
+    height: "30px",
+    width: "350px",
+    border: "6px outset LightGray",
+    borderRadius: "100px",
+    fontSize: "25px",
+    marginRight: "25px",
+    color: "MediumSlateBlue"
+}
+
+const buttonStyle = {
+    height: "35px",
+    width: "100px",
+    border: "3px groove LightSkyBlue",
+    backgroundColor: "LightSkyBlue",
+    borderRadius: "10px",
+    fontSize: "15px",
+    color: "blue",
+    cursor: "pointer",
+    marginRight: "25px"
+}
+
+const usersListStyle = {
+    width: "200px",
+    color: "green"
+}
+
+const userStyle = {
+    color: "black"
 }
--- chat-room/src/logPage.js
+++ chat-room/src/logPage.js
@@ -19,9 +19,40 @@ export default function LogPage(props) {
                 body: JSON.stringify({"user": name.current.value})
             })
     }
-    return <div>
-        <h1>welcome to chat room</h1>
-        <input ref={name} placeholder='enter your name'></input>
-        <button onClick={logIn}><Link to='/chat'>go to chat</Link></button>
+    return <div style={logStyle}>
+        <h1>chat room</h1>
+        <input style={inputStyle} ref={name} placeholder='user name'></input>
+        <button style={buttonStyle} onClick={logIn}><Link style={linkStyle} to='/chat'>start chating</Link></button>
     </div>
+}
+
+const linkStyle = {
+    textDecoration: "none",
+    color: "blue"
+}
+
+const logStyle = {
+    textAlign: "center",
+    paddingTop: "50px",
+    fontSize: "45px",
+    color: "LightSkyBlue"
+}
+
+const inputStyle = {
+    height: "30px",
+    width: "200px",
+    border: "6px outset LightGray",
+    borderRadius: "100px",
+    fontSize: "25px",
+    marginRight: "25px",
+    color: "MediumSlateBlue"
+}
+
+const buttonStyle = {
+    height: "35px",
+    width: "100px",
+    border: "3px groove LightSkyBlue",
+    backgroundColor: "LightSkyBlue",
+    borderRadius: "10px",
+    fontSize: "15px"
 }
